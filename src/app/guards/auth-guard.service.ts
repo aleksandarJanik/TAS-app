45ad60b5b32c5d01d20f12eef580344df5b6b06e
NorthBL <gjorgjievskisvetog@gmail.com>
2021-11-15 15:57:18 +0100

    added child guard for create-account routes

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot, CanActivateChild, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 import { LogInService } from '../login/components/login/logIn.service';
  
  
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild {
  
     constructor(private _router:Router, private loginService: LogInService ) {
+    }
+
+    async canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+        let sessions = await this.loginService.getSessions();
+        //check some condition  
+        if (sessions)  {
+            let userName = localStorage.getItem("userName");
+            if (userName){
+               let userInSession = sessions.find(u => u.sessionId.includes(userName));
+               if (userInSession) {
+                alert('You are Already Logged in.');
+                this._router.navigateByUrl("/class");
+                return false;
+               }
+            }
+            return true;
+        } 
+        return true;
     }
  
     async canActivate(route: ActivatedRouteSnapshot,
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuardService } from "../guards/auth-guard.service";
 import { CreateAccountComponent } from "./components/create-account/create-account.component";
 import { LoginComponent } from "./components/login/login.component";
 import { SignUpComponent } from "./components/sign-up/sign-up.component";
@@ -8,6 +9,7 @@ const routes: Routes = [
   {
     path: "create-account",
     component: CreateAccountComponent,
+    canActivateChild : [AuthGuardService],
     children: [
       { path: "", redirectTo: "login", pathMatch: "full" },
       { path: "login", component: LoginComponent },
