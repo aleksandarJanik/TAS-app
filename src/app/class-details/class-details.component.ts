df0f9b6eaab8f0cfd8c07450f04ec026c52fa92d
NorthBL <gjorgjievskisvetog@gmail.com>
2021-11-17 16:59:36 +0100

    added check box and pick student for lecturing

--- src/app/class-details/class-details.component.ts
+++ src/app/class-details/class-details.component.ts
@@ -8,6 +8,7 @@ import Swal from "sweetalert2";
 import { StringLiteralLike } from "typescript";
 import { LogInService } from "../login/components/login/LogIn.service";
 import { Class } from "../models/class";
+import { StartLecturing } from "../models/startLecturing";
 import { Student } from "../models/student";
 import { ClassService } from "../services/class.service";
 import { StudentService } from "../services/student.service";
@@ -22,7 +23,13 @@ export class ClassDetailsComponent implements OnInit {
   classes: Class[];
   students: Student[];
   currentClass: Class;
+  startLecturing: StartLecturing[];
+  presentStudents: StartLecturing[];
+  copyPresentStudents: StartLecturing[];
+  pickedStudent: StartLecturing;
+  pickedStudentName: string;
   studentNameSurname: string;
+  counter: number = 0;
   public show: boolean = true;
 
   constructor(
@@ -39,7 +46,12 @@ export class ClassDetailsComponent implements OnInit {
       this.students = await this.studentService.getStudentsByClass(
         this.classId
       );
-      console.log("test" + JSON.stringify(this.students));
+      this.startLecturing = this.students.map( s => ({
+        student: s,
+        isPresent: true,
+        isPicked: false
+      }) as StartLecturing)
+      // console.log("start lecturing Object " + JSON.stringify(this.startLecturing));
     }
   }
 
@@ -92,5 +104,27 @@ export class ClassDetailsComponent implements OnInit {
       text: "The student successfully removed!!",
       icon: "success",
     });
+  }
+
+  checkValue(student: StartLecturing): void {
+    student.isPresent = !!student.isPresent;
+  }
+
+  pickStudent(): void {
+    let removed: StartLecturing[];
+    if (this.counter === 0) {
+      this.presentStudents = this.startLecturing.filter( s => s.isPresent === true);
+      this.copyPresentStudents = [...this.presentStudents];
+      this.counter++;
+    }
+    const rndInt = Math.floor(Math.random() * this.presentStudents.length) + 1;
+    this.pickedStudent = this.presentStudents[rndInt - 1];
+    this.pickedStudentName = this.pickedStudent.student.name;
+    if (this.presentStudents.length === 1){
+      this.presentStudents = [];
+    } else {
+      this.presentStudents = this.presentStudents.splice(rndInt - 1, 1);
+    }
+    console.log("arrayyyyyy " + JSON.stringify(this.presentStudents));
   }
 }
---
+++ src/app/models/startLecturing.ts
@@ -0,0 +1,7 @@
+import { Student } from "./student";
+
+export class StartLecturing {
+    student: Student;
+    isPresent: boolean;
+    isPicked: boolean;
+}
