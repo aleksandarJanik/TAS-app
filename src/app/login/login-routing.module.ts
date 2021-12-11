f4ffdd7378f3960a82785a0556b5833ea5f0f158
aleksandar34 <aleksandar.janik34@gmail.com>
2021-12-11 11:09:49 +0100

    Add info in app

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuardService } from "../guards/auth-guard.service";
+import { ChangePasswordComponent } from "./components/change-password/change-password.component";
 import { CreateAccountComponent } from "./components/create-account/create-account.component";
 import { LoginComponent } from "./components/login/login.component";
 import { SignUpComponent } from "./components/sign-up/sign-up.component";
@@ -9,11 +10,12 @@ const routes: Routes = [
   {
     path: "create-account",
     component: CreateAccountComponent,
-    canActivateChild : [AuthGuardService],
+    canActivateChild: [AuthGuardService],
     children: [
       { path: "", redirectTo: "login", pathMatch: "full" },
       { path: "login", component: LoginComponent },
       { path: "sign-up", component: SignUpComponent },
+      { path: "change-password", component: ChangePasswordComponent },
     ],
   },
 ];
